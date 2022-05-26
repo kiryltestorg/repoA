e470057d5e701a61eebb41afc2737d11d57037f2
Kul <kirylkul@bcd0741114c3.ant.amazon.com>
2022-05-26 15:25:52 -0700

    added repoA class

---
+++ src/main/java/org/example/repo.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class repo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void repoA(){
+        System.out.println("printing from repoA");
+
+    }
+
+}
